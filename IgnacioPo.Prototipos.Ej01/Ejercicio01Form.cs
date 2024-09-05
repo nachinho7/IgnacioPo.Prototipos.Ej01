9324af19508c1d42918ebc1a162d2f481b43f111
nchp7 <nacho.porretta00@gmail.com>
2024-09-05 20:30:45 -0300

    Add project files.

---
+++ IgnacioPo.Prototipos.Ej01/Ejercicio01Form.cs
@@ -0,0 +1,15 @@
+namespace IgnacioPo.Prototipos.Ej01
+{
+    public partial class Ejercicio01Form : Form
+    {
+        public Ejercicio01Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Ejercicio01Form_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
